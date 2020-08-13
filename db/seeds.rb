0f82693dc511285f20c18a51638e74667a3d03bc
Corentin Malvaux <corentin.malvaux@gmail.com>
2020-08-13 11:58:38 +0200

    populated ingredient table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "caca")
+Ingredient.create(name: "fraises")
+Ingredient.create(name: "chocolat")
+Ingredient.create(name: "frites")
+Ingredient.create(name: "table")
+Ingredient.create(name: "chaise")
